884fd6d5b293d088517577e93ebda5e21863017c
lofcgo1205@gmail.com <lofcgo1205@gmail.com>
2025-05-29 00:44:17 +0900

    [Refactor] HeaderWrapper 함수에서 cookies() 호출을 비동기로 변경하여 세션 처리 일관성 향상

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,9 +1,9 @@
 import { cookies } from "next/headers";
 import Header from "./header-client";
 
-export default function HeaderWrapper() {
-  const cookieStore = cookies();
-  const session = cookieStore.get("connect.sid");
+export default async function HeaderWrapper() {
+  const cookieStore = await cookies();
+  const session = await cookieStore.get("connect.sid");
   const isLoggedIn: boolean = !!session;
   return <Header isLoggedIn={isLoggedIn} />;
 }
